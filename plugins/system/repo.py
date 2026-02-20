91b4744488006a1c7b21fdec9a7722b19e9b80fc
Arsalan <35004378+a092devs@users.noreply.github.com>
2026-02-21 03:14:18 +0530

    feat(system): add official repo

---
+++ plugins/system/repo.py
@@ -0,0 +1,31 @@
+from utils.respond import respond
+
+
+__plugin__ = {
+    "name": "Repository",
+    "category": "system",
+    "description": (
+        "Show the official Atlas repository link.\n\n"
+        "Usage:\n"
+        "`.repo`"
+    ),
+    "commands": {
+        "repo": "Show repository link",
+    },
+}
+
+
+REPO_URL = "https://github.com/a092devs/Atlas-Userbot"
+
+
+async def handler(event, args):
+    text = (
+        "<b>Atlas Userbot</b>\n\n"
+        f"<a href='{REPO_URL}'>GitHub Repository</a>"
+    )
+
+    await respond(
+        event,
+        text,
+        link_preview=False
+    )
